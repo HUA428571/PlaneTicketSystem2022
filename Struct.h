a4456547b82ebbaff069e108979bed1421b75f9b
HuaClark <106476357+HuaClark@users.noreply.github.com>
2022-05-30 14:17:59 +0800

    initial push. prepare for dbtest3 --HuaCL 20220529.2236

--- Struct.h
+++ Struct.h
@@ -18,11 +18,11 @@ using namespace std;
 
 typedef struct FlightID //用于表示航班号，航班号只代表航班基本信息。
 {
-	char Carrier[4] = { 'X','X','X','\0' };									//航空公司，用两个大写字母表示
+	char Carrier[3] = { 'X','X','\0' };										//航空公司，用两个大写字母表示
 	char ID[8] = { 'X','X','X','X','X','X','X','\0' };						//航班号
 	char FlyDay[9] = { 'X','X','X','X','X','X','X','X','\0' };				//开航日期，以星期为周期，星期几与数组下标对应，开航为数字，不开航为0
-	char DepartureAirport[4] = { 'X','X','X','\0' };						//出发机场，三字码，为标明航站楼等，以字符串形式存储，注意结尾的\0
-	char ArrivalAirport[4] = { 'X','X','X','\0' };							//抵达机场，三字码，为标明航站楼等，以字符串形式存储，注意结尾的\0
+	char DepartureAirport[5] = { 'X','X','X','X','\0' };					//出发机场，三字码，为标明航站楼等，以字符串形式存储，注意结尾的\0
+	char ArrivalAirport[5] = { 'X','X','X','X','\0' };						//抵达机场，三字码，为标明航站楼等，以字符串形式存储，注意结尾的\0
 	int DepartureTime = 2500;												//出发时刻，四字码
 	int ArrivalTime = 2500;													//到达时刻，四字码。eg 1230（12时30分）
 	int TravelTimeHour = 0;													//飞行时间(时)
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include"CustomMenu.h"
 #include"StartMENU.h"
 #include<stdlib.h>
+#include<mysql.h>
 using namespace std;
 //主函数
 int main()
@@ -39,12 +40,50 @@ int main()
 		Sleep(1000);
 		exit(1);
 	}
-	int Choice = StartMENU(U, P);
+	//int Choice = StartMENU(U, P);
 	//创建绘图窗口，大小为1280x720像素，没有关闭按钮
-	initgraph(1280, 720, EW_NOCLOSE);
-	if (Choice == 0)
-		AdminMENU(ID, DATA, FlightID_Count);
-	else
-		CustomMenu(ID, DATA, FlightID_Count, P, U, FO, &custom, Choice);
+//	initgraph(1280, 720, EW_NOCLOSE);
+	//if (Choice == 0)
+//		AdminMENU(ID, DATA, FlightID_Count);
+	//else
+	//	CustomMenu(ID, DATA, FlightID_Count, P, U, FO, &custom, Choice);
+
+	MYSQL mysql;    //数据库句柄
+	MYSQL_RES* res; //查询结果集
+	MYSQL_ROW row;  //记录结构体
+
+	//初始化数据库
+	mysql_init(&mysql);
+
+	//设置字符编码
+	mysql_options(&mysql, MYSQL_SET_CHARSET_NAME, "gbk");
+
+	//连接数据库
+	if (mysql_real_connect(&mysql, "127.0.0.1", "B19030314", "B19030314", "dbexp3", 3306, NULL, 0) == NULL) {
+		printf("错误原因： %s\n", mysql_error(&mysql));
+		printf("连接失败！\n");
+		exit(-1);
+	}
+
+	//查询数据
+	int ret = mysql_query(&mysql, "select * from flightid;");
+	printf("ret: %d\n", ret);
+
+	//获取结果集
+	res = mysql_store_result(&mysql);
+
+	//给ROW赋值，判断ROW是否为空，不为空就打印数据。
+	while (row = mysql_fetch_row(res))
+	{
+		printf("%s  ", row[0]);  //打印ID
+		printf("%s  ", row[1]);  //打印班级
+		printf("%s\n", row[2]);  //打印姓名
+	}
+	//释放结果集
+	mysql_free_result(res);
+
+	//关闭数据库
+	mysql_close(&mysql);
+
 	return 0;
 }
